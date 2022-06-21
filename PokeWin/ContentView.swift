3b4ad04de6eb8c03b2d84c437623a0f4aca3b178
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2022-06-21 11:31:19 +0800

    Initial Commit

---
+++ PokeWin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeWin
+//
+//  Created by caramel melmel on 21/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeWin/PokeWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeWinApp.swift
+//  PokeWin
+//
+//  Created by caramel melmel on 21/6/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
