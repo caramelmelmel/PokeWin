e09e98b4d39ea8d3a96a5cfe279c32c54a07e0d2
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2022-06-22 10:37:17 +0800

    added all frontend codes

--- PokeWin/ContentView.swift
+++ PokeWin/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    @State var counter = 0
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        VStack {
+            // Bulbasaur
+            Image("bulbasaur").resizable().aspectRatio(contentMode: .fit).padding(18.0)
+            // counter display
+            Text("Counter value: \(self.counter)")
+                .font(.headline)
+                .foregroundColor(.blue)
+                .padding(14.0)
+
+            // button
+            HStack {
+                Button("Caught My Pokemon", action: {
+                    self.counter += 1
+                })
+                .padding(10.0).overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.blue, lineWidth: 2)
+                )
+
+            }
+        }
+    }
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
